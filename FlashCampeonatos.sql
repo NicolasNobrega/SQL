d2ba0e9a31d57a4af1afdd7ed61915bb5ec61cfd
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-04-11 20:32:44 -0300

    Organized Team by Key

--- FlashCampeonatos.sql
+++ FlashCampeonatos.sql
@@ -10,13 +10,34 @@ CREATE TABLE usuarios (
   time_id INT
 );
 
-INSERT INTO usuarios (nome, elo, time_id) VALUES ('Usuário 1', 'Prata', 1);
+INSERT INTO usuarios (nome, elo, time_id) VALUES 
+('Usuário 1', 'Ferro', 1),
+('Usuário 2', 'Ferro', 1),
+('Usuário 3', 'Ferro', 1),
+('Usuário 4', 'Ferro', 1),
+('Usuário 5', 'Ferro', 1),
+
+('Usuário 6', 'Ferro', 2),
+('Usuário 7', 'Ferro', 2),
+('Usuário 8', 'Ferro', 2),
+('Usuário 9', 'Ferro', 2),
+('Usuário 10', 'Ferro', 2);
 
 -- Criação da tabela de chaves
 CREATE TABLE chaves (
   id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(100) NOT NULL
 );
+
+-- Insere os dados de exemplo na tabela de chaves
+INSERT INTO chaves (nome) VALUES 
+('Ferros'),
+('Bronzes'),
+('Prata'),
+('Ouro'),
+('Platina'),
+('Diamante'),
+('Mestre');
 
 -- Criação da tabela de times
 CREATE TABLE times (
@@ -28,23 +49,20 @@ CREATE TABLE times (
 
 
 -- Criação da tabela de usuários em uma chave (relacionamento many-to-many)
-CREATE TABLE usuarios_chave (
+CREATE TABLE times_chave (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  usuario_id INT,
+  time_id INT,
   chave_id INT,
-  FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+  FOREIGN KEY (time_id) REFERENCES times(id),
   FOREIGN KEY (chave_id) REFERENCES chaves(id)
 );
+INSERT INTO times_chave (time_id, chave_id) VALUES 
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5,2);
 
--- Insere os dados de exemplo na tabela de chaves
-INSERT INTO chaves (nome) VALUES ('Chave 1'), ('Chave 2');
+select * from times_chave where chave_id = 1;
 
--- Insere os dados de exemplo na tabela de times, atribuindo-os a uma chave
-INSERT INTO times (nome, chave_id) VALUES ('Time A', 1), ('Time B', 1), ('Time C', 2);
-
--- Exemplo de inserção de um usuário com elo e atribuição a um time
-
-
--- Exemplo de inserção de um usuário em uma chave
-INSERT INTO usuarios_chave (usuario_id, chave_id) VALUES (1, 1);
 
